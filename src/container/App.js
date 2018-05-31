3521caa943110164eb09c2e00ad01e8197086755
admin <admin@admins-Mac-mini-2.local>
2018-05-31 14:21:31 +0700

    add threejs to app

--- src/container/App.js
+++ src/container/App.js
@@ -1,15 +1,39 @@
 import React, { Component } from 'react';
 import logo from '../assets/logo.svg';
 import '../styles/App.css';
+import React3 from 'react-three-renderer';
 import * as THREE from 'three';
+import OrbitControls from '../util/OrbitControls'
 
+let scene, camera, renderer, controls;
 class App extends Component {
   constructor(props){
     super(props);
-    debugger
+    this.init = this.init.bind(this)
+    this.animate = this.animate.bind(this)
+  }
+  componentDidMount() {
+    this.init()
+    this.animate()
   }
   init() {
+    let width = window.innerWidth / 2;
+    let height = window.innerHeight / 2;
+    scene = new  THREE.Scene();
+    camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+    renderer = new THREE.WebGLRenderer();
+    renderer.setSize( window.innerWidth/2, window.innerHeight/2 );
+    document.getElementById('3d-rendering').appendChild( renderer.domElement );
+    camera = new THREE.PerspectiveCamera(45, width / height, 0.1, 20000);
+    camera.position.set(0,6,0);
+    controls = new THREE.OrbitControls(camera, renderer.domElement);
+    scene.add(camera);
+  }
 
+  animate() {
+    // requestAnimationFrame("animate");
+    renderer.render(scene, camera);
+    controls.update();
   }
   
   render() {
@@ -22,6 +46,8 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <div id='3d-rendering'>
+        </div>
       </div>
     );
   }
--- src/util/OrbitControls.js
+++ src/util/OrbitControls.js
@@ -12,7 +12,7 @@
 //    Orbit - left mouse / touch: one-finger move
 //    Zoom - middle mouse, or mousewheel / touch: two-finger spread or squish
 //    Pan - right mouse, or arrow keys / touch: two-finger move
-
+import * as THREE from 'three';
 THREE.OrbitControls = function ( object, domElement ) {
 
 	this.object = object;
