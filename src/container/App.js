0b5023b57a3405118f101869c874067342e44353
admin <admin@admins-Mac-mini-2.local>
2018-06-01 15:23:59 +0700

    add model to app

--- src/container/App.js
+++ src/container/App.js
@@ -4,48 +4,60 @@ import '../styles/App.css';
 // import React3 from 'react-three-renderer';
 import * as THREE from 'three'
 import OrbitControls from '../util/OrbitControls.js';
-let scene, camera, renderer, controls;
+
+let scene, camera, renderer, controls, mesh;
+let loader = new THREE.JSONLoader();
+let textureLoader = new THREE.TextureLoader();
 const WIDTH = window.innerWidth/2;
 const HEIGHT = window.innerHeight/2;
+
 class App extends Component {
   constructor(props){
     super(props);
     this.init = this.init.bind(this)
     this.animate = this.animate.bind(this)
   }
+
   componentDidMount() {
     this.init()
     this.animate()
   }
+  
   init() {
     scene = new  THREE.Scene();
     renderer = new THREE.WebGLRenderer();
     renderer.setSize( WIDTH, HEIGHT);
-    scene.background = new THREE.Color( 0x242a34 );
+    scene.background = new THREE.Color(0x754E46);
     document.getElementById('3d-rendering').appendChild( renderer.domElement );
     this.addCamera();
     this.addMaterial();
   }
 
   addMaterial(){
-    let material = new THREE.MeshBasicMaterial();
-    let mesh = new THREE.Mesh( new THREE.BoxBufferGeometry( 200, 200, 200 ), material );
-    debugger
-    scene.add( mesh );
+    loader.load( '/models/leeper/LeePerrySmith.json', function( geometry ) {
+      let material = new THREE.MeshPhongMaterial( {
+        specular: 0x111111,
+        map: textureLoader.load( 'models/leeper/Map-COL.jpg' ),
+        specularMap: textureLoader.load( 'models/leeper/Map-SPEC.jpg' ),
+        normalMap: textureLoader.load( 'models/leeper/Infinite-Level_02_Tangent_SmoothUV.jpg' ),
+        shininess: 25
+      } );
+      mesh = new THREE.Mesh( geometry, material );
+      scene.add( mesh );
+      mesh.scale.set( 10, 10, 10 );
+    } );
   }
 
   addCamera() {
-    camera = new THREE.PerspectiveCamera(45, WIDTH / HEIGHT, 0.1, 20000);
+    camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000);
     camera.position.set(0,6,0);
     controls = new OrbitControls(camera, renderer.domElement);
+    controls.update();
     scene.add(camera);
   }
 
   animate() {
-    // requestAnimationFrame("animate");
-    debugger
     renderer.render(scene, camera);
-    controls.update();
   }
   
   render() {
